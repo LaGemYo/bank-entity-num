e1b3a9c2d9546cd9fb48a9a550fd952e5a7a7efe
LaGemYo <lagemyo@gmail.com>
2019-09-02 17:36:37 +0200

    Started to work on SearchBar styles.

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,24 +1,35 @@
 import React from "react";
 import styled from "styled-components";
-
+import { Button } from 'react-bootstrap';
 
 export default function SearchBar() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col col-6">
-          <div>
-            Code:
-          <input id="bank_code" type="text" />
+    <searchBarWrapper>
+      <div className="container">
+        <div className="row">
+          <div className="col col-6">
+            <div className="my-4">
+              <label className="mr-20">Code:</label>
+              <input className="my-2" id="bank_code" type="text" />
+            </div>
           </div>
-        </div>
-        <div className="col col-6">
-          <div>
-            Name:
-          <input id="bank_name" type="text" />
+          <div className="col col-6">
+            <div className="my-4">
+              <label>Name:</label>
+              <input className="my-2" id="bank_name" type="text" />
+            </div>
+          </div>
+          <div className="col col-4">
+            <Button type="button" className="btn-info">
+              Search
+            </Button>
           </div>
         </div>
       </div>
-    </div>
+    </searchBarWrapper>
   )
 }
+
+const searchBarWrapper = styled.div`
+  
+`;
