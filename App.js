a41b658b22b64dda88836e511f0fa38c725786ed
LaGemYo <lagemyo@gmail.com>
2019-09-02 13:43:28 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import { Route, Switch } from 'react-router-dom';
+
+
+
+class App extends Component {
+    render() {
+    return (
+      <>
+<h1>hello from app.js</h1>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# bank-entity-num
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/bank-entity-num)
